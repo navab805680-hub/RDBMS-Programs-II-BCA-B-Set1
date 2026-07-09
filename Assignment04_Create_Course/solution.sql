17cdb71cbaab069ffd81a4c5bc3a097857f120e5
navab805680-hub <navab805680@gmail.com>
2026-07-09 12:40:35 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,4 @@
--- Create Course table
-
--- Insert three records
-
--- Display structure
+USE collegeDBBCA;
+CREATE TABLE Course (courseID VARCHAR(20),courseNAME VARCHAR(20) PRIMARY KEY,DepartmentID VARCHAR(20));
+INSERT INTO Course (courseID,courseName,credits,DepartmentID)VALUE (10,"BCA",12,1);
+DESC Course;
