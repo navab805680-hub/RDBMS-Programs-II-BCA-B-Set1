1d2555e0d0c57f2c64fa1197a632ed7756ad95ca
navab805680-hub <navab805680@gmail.com>
2026-07-17 11:33:22 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,4 @@
--- Create Course table
-
--- Insert three records
-
--- Display structure
+Create database college_db;
+use college_db;
+CREATE TABLE department (department_id INT PRIMARY KEY, department_name VARCHAR(100), HOD VARCHAR(100));
+SELECT* FROM DEPARTMENT;
